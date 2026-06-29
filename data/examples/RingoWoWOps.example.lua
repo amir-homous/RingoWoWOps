476f678d74f73706986292ae1b62550399ceaf7e
Amir Hossein Mousavi <amir.homos@gmail.com>
2026-06-29 09:23:25 -0700

    test: add example SavedVariables fixture

---
+++ data/examples/RingoWoWOps.example.lua
@@ -0,0 +1,65 @@
+RingoWoWOpsDB = {
+  version = "0.1.0",
+  sessions = {
+    {
+      id = "test-001",
+      character = "Ringo",
+      realm = "Gehennas",
+      faction = "Horde",
+      class = "PALADIN",
+      level_start = 1,
+      level_end = 2,
+      zone_start = "Sunstrider Isle",
+      zone_end = "Sunstrider Isle",
+      xp_start = 0,
+      xp_end = 850,
+      gold_start = 0,
+      gold_end = 120,
+      started_at = 1780000000,
+      ended_at = 1780003600,
+      duration_seconds = 3600,
+      activity_start = "questing",
+      activity_end = "questing"
+    }
+  },
+  snapshots = {
+    {
+      time = 1780000100,
+      reason = "manual",
+      character = "Ringo",
+      realm = "Gehennas",
+      faction = "Horde",
+      class = "PALADIN",
+      level = 1,
+      zone = "Sunstrider Isle",
+      subzone = "Sunstrider Isle",
+      xp = 100,
+      gold = 20,
+      bags_free = 14,
+      activity = "questing"
+    }
+  },
+  notes = {
+    {
+      time = 1780000200,
+      character = "Ringo",
+      realm = "Gehennas",
+      level = 1,
+      zone = "Sunstrider Isle",
+      activity = "questing",
+      text = "First test note"
+    }
+  },
+  activities = {
+    {
+      time = 1780000050,
+      character = "Ringo",
+      realm = "Gehennas",
+      level = 1,
+      zone = "Sunstrider Isle",
+      activity = "questing"
+    }
+  },
+  loot = {},
+  settings = {}
+}
